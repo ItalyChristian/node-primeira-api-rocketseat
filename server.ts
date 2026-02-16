515c4f3b5e01e5e08b5e8adaff71eec981f1cd39
italy <italy.christian@hotmail.com>
2026-02-16 15:32:58 -0300

    first commit

---
+++ server.ts
@@ -0,0 +1,64 @@
+import fastify from "fastify"
+import crypto from "node:crypto" // esse "node" na frente é opcional dessa importação. mas está sendo usado pra evidenciar q é uma dependencia natural do node
+
+const server = fastify({
+  logger: {
+    transport: {
+      target: 'pino-pretty',
+      options: {
+        translateTime: 'HH:MM:ss Z',
+        ignore: 'pid,hostname',
+      },
+    },
+  }
+})
+
+const courses = [
+  { id: '1', title: 'Curso de Node.js' },
+  { id: '2', title: 'Curso de React.js' },
+  { id: '3', title: 'Curso de Next.js' },
+]
+
+server.get('/courses', () => {
+  return { courses }
+})
+
+server.get('/courses/:id', (request, reply) => {
+  type Params = {
+    id: string
+  }
+  
+  const params = request.params as Params
+  const courseId = params.id
+  
+  const course = courses.find(course => course.id === courseId)
+
+  if(course) {
+    return { course }
+  }
+  
+  return reply.status(404).send()
+})
+
+server.post('/courses', (request, reply) => { 
+  type Body = { 
+    title: string
+  }
+  
+  const courseId = crypto.randomUUID()
+
+  const body = request.body as Body
+  const courseTitle = body.title
+
+  if(!courseTitle) {
+    return reply.status(400).send({ message: 'Título obrigatório' })
+  }
+
+  courses.push({ id: courseId, title: courseTitle })
+
+  return reply.status(201).send({ courseId })
+})
+
+server.listen({ port: 3333 }).then(() => {
+  console.log('HTTP server running!')
+})
