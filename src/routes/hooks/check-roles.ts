ecfd3d6bd8446eeb2d84513066c7896f555afdc1
italy <italy.christian@hotmail.com>
2026-04-22 21:54:53 -0300

    updating enum to types

--- src/routes/hooks/check-roles.ts
+++ src/routes/hooks/check-roles.ts
@@ -1,8 +1,8 @@
 import type { FastifyRequest, FastifyReply } from "fastify";
 import { getAuthenticatedFromRequest } from "../../utils/get-authenticated-user-from-request.ts";
-import { USER_ROLE } from "../../utils/types.ts";
+import type { UserRole } from "../../utils/types.ts";
 
-export function checkUserRole(role: USER_ROLE) {
+export function checkUserRole(role: UserRole) {
   return async function (request: FastifyRequest, reply: FastifyReply) {
     const user = getAuthenticatedFromRequest(request);
 
--- src/utils/types.ts
+++ src/utils/types.ts
@@ -1,4 +1,6 @@
-export enum USER_ROLE {
-  MANAGER = 'manager',
-  STUDENT = 'student'
-}
+export const USER_ROLE = {
+  MANAGER: 'manager',
+  STUDENT: 'student',
+} as const;
+
+export type UserRole = (typeof USER_ROLE)[keyof typeof USER_ROLE];
